f28e0808d7d4eb2900850ced630bb21918c2b59f
KlaytonJr <klaytonjunior@protonmail.com>
2023-08-27 16:33:36 -0300

    Use Middleware with UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -22,66 +22,68 @@ class UserController extends Controller {
     this.router.delete(`${this.path}/:id`, this.delete);
   }
 
-  private async list(req: Request, res: Response, next: NextFunction): Promise<Response> {
+  private async list(req: Request, res: Response, next: NextFunction): Promise<Response | any> {
     try {
       const users = await User.find();
 
-      return responseOk(res, users);
+      if (users.length) return responseOk(res, users);
+
+      next(new NoContentException());
     } catch (error) {
-      return res.send(new ServerErrorException(error));
+      next(new ServerErrorException(error));
     }
   }
 
-  private async findById(req: Request, res: Response, next: NextFunction): Promise<Response> {
+  private async findById(req: Request, res: Response, next: NextFunction): Promise<Response | any> {
     try {
       const { id } = req.params;
-
-      if (ValidationService.validateId(id, next)) return res.status(HttpStatusCode.BAD_REQUEST).send(new IdInvalidException());
+      if (ValidationService.validateId(id, next)) return;
 
       const user = await User.findById(id);
-
-      return responseOk(res, user);
+      if (user) return responseOk(res, user);
+      next(new NoContentException());
     } catch (error) {
-      return res.send(new ServerErrorException(error));
+      next(new ServerErrorException(error));
     }
   }
 
-  private async create(req: Request, res: Response, next: NextFunction): Promise<Response> {
+  private async create(req: Request, res: Response, next: NextFunction): Promise<Response | any> {
     try {
       const user = await User.create(req.body);
 
       return responseCreate(res, user);
     } catch (error) {
-      return res.send(new ServerErrorException(error));
+      next(new ServerErrorException(error));
     }
   }
 
-  private async edit(req: Request, res: Response, next: NextFunction): Promise<Response> {
+  private async edit(req: Request, res: Response, next: NextFunction): Promise<Response | any> {
     try {
       const { id } = req.params;
-      if (ValidationService.validateId(id, next)) return res.status(HttpStatusCode.BAD_REQUEST).send(new IdInvalidException());
+      if (ValidationService.validateId(id, next)) return;
 
       const user = await User.findByIdAndUpdate(id, req.body, () => {});
+      if (user) return responseOk(res, user);
 
-      return responseOk(res, user);
+      next(new NoContentException());
     } catch (error) {
-      return res.send(new ServerErrorException(error));
+      next(new ServerErrorException(error));
     }
   }
 
-  private async delete(req: Request, res: Response, next: NextFunction): Promise<Response> {
+  private async delete(req: Request, res: Response, next: NextFunction): Promise<Response | any> {
     try {
       const { id } = req.params;
-      if (ValidationService.validateId(id, next)) return res.status(HttpStatusCode.BAD_REQUEST).send(new IdInvalidException());
+      if (ValidationService.validateId(id, next)) return;
 
       const user = await User.findById(id, req.body, () => {});
       if (user) {
         user.deleteOne();
         return responseOk(res, user);
       }
-      return res.status(HttpStatusCode.NO_CONTENT).send(new NoContentException());
+      next(new NoContentException());
     } catch (error) {
-      return res.send(new ServerErrorException(error));
+      next(new ServerErrorException(error));
     }
   }
 }
